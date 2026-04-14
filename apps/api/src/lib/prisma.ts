144b2d96a591e037246092e37d5cbfb6d4eca82b
irlangaragem <irlan@garagem.dev.br>
2026-04-14 11:30:21 -0300

    chore: logs ultra-limpos (sem query select 1, sem npm notice)

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -7,7 +7,7 @@ declare global {
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ['query'],
+    log: process.env.NODE_ENV === 'production' ? ['error'] : ['query', 'error', 'warn'],
   });
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
